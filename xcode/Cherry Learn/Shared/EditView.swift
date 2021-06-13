1f66616bcf1f05aed856fa1b2ad81de96e350f6b
faradawn <faradawny@gmail.com>
2021-06-13 22:36:48 +0800

    edit view finished

---
+++ xcode/Cherry Learn/Shared/EditView.swift
@@ -0,0 +1,8 @@
+//
+//  EditView.swift
+//  Cherry Learn
+//
+//  Created by faradawn on 2021/6/13.
+//
+
+import Foundation
---
+++ xcode/Cherry Learn/Shared/UserDetailedView.swift
@@ -0,0 +1,8 @@
+//
+//  UserDetailedView.swift
+//  Cherry Learn
+//
+//  Created by faradawn on 2021/6/13.
+//
+
+import Foundation
